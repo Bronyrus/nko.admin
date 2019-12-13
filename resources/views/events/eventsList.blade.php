72b0287bb7e0ddc8c85a904f081031b5242f6d85
Bronyrus <bronyruschannel@gmail.com>
2019-12-13 12:31:48 +0300

    Update eventsList.blade.php

--- resources/views/events/eventsList.blade.php
+++ resources/views/events/eventsList.blade.php
@@ -1,38 +1,34 @@
 @extends('layouts.adminApp')
 
 @section('content')
-    
-    <a href="{{ route('auth.cases.create') }}" class="btn btn-primary">Добавить кейс</a>
 
-    <br>
-
-    <div class="col-sm-12 tabs-content">
-        <div class="row justify-content-center cont-m">
-            <div class="col-md-12">
-                <div class="group-btn-card">
-                    <a href="{{ route('auth.events.create') }}" class="btn-card">Создать событие</a>
-                    <button type="button" class="btn-card btn-tc-danger js-destroy-button">Удалить отмеченные события</button>
-                </div>
-                <table class="table policy-table">
-                    <thead>
-                    <tr>
-                        <th scope="col"><input type="checkbox" name="destroy-all-polls" class="js-destroy-all"/></th>
-                        <th scope="col">Наименование</th>
-                        <th scope="col">Дата начала</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {{-- @foreach($polls as $poll)
-                        <tr>
-                            <td scope="row"><input type="checkbox" data-poll-id="{{ $poll->id }}" name="destoy-poll-{{ $poll->id }}" class="js-destroy"/></td>
-                            <td><a href="poll/{{ $poll->id }}"> {{ $poll->name }} </a></td>
-                            <td>{{ $poll->created_at->timezone('Europe/Moscow') }}</td>
-                        </tr>
-                    @endforeach --}}
-                    </tbody>
-                </table>
+<div class="col-sm-12 tabs-content">
+    <div class="row justify-content-center cont-m">
+        <div class="col-md-12">
+            <div class="group-btn-card">
+                <a href="{{ route('auth.events.create') }}" class="btn-card">Создать событие</a>
+                <button type="button" class="btn-card btn-tc-danger js-destroy-button">Удалить отмеченные события</button>
             </div>
+            <table class="table policy-table">
+                <thead>
+                <tr>
+                    <th scope="col"><input type="checkbox" name="destroy-all-polls" class="js-destroy-all"/></th>
+                    <th scope="col">Наименование</th>
+                    <th scope="col">Дата начала</th>
+                </tr>
+                </thead>
+                <tbody>
+                {{-- @foreach($polls as $poll)
+                    <tr>
+                        <td scope="row"><input type="checkbox" data-poll-id="{{ $poll->id }}" name="destoy-poll-{{ $poll->id }}" class="js-destroy"/></td>
+                        <td><a href="poll/{{ $poll->id }}"> {{ $poll->name }} </a></td>
+                        <td>{{ $poll->created_at->timezone('Europe/Moscow') }}</td>
+                    </tr>
+                @endforeach --}}
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
 
 @endsection

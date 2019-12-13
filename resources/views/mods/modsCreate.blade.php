dadef6b7e8f0ed403d21a43a0d52df24d6318747
Bronyrus <bronyruschannel@gmail.com>
2019-12-13 15:27:20 +0300

    Update modsCreate.blade.php

--- resources/views/mods/modsCreate.blade.php
+++ resources/views/mods/modsCreate.blade.php
@@ -27,7 +27,7 @@
         </div>
 
         <div class="col-xs-12 col-sm-10">
-            <textarea id="email" type="email" class="md-textarea form-control" name="email" cols="30" rows="10" maxlength="2000">{{ old('email') }}</textarea>
+            <input type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
             @if ($errors->has('email'))
                 <span class="help-block">
@@ -43,7 +43,7 @@
         </div>
 
         <div class="col-xs-12 col-sm-10">
-            <input type="date" class="form-control" name="password" value="{{ old('password') }}" required>
+            <input type="password" class="form-control" name="password" value="{{ old('password') }}" required>
 
             @if ($errors->has('password'))
                 <span class="help-block">
@@ -59,7 +59,7 @@
         </div>
 
         <div class="col-xs-12 col-sm-10">
-            <input type="date" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}" required>
+            <input type="password" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}" required>
 
             @if ($errors->has('password_confirmation'))
                 <span class="help-block">

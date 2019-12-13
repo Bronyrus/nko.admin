43dffbedb4b1537c753c087d6f8903900419fbbc
Bronyrus <bronyruschannel@gmail.com>
2019-12-13 10:27:18 +0300

    Update caseCreate.blade.php

--- resources/views/cases/caseCreate.blade.php
+++ resources/views/cases/caseCreate.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     
-<form class="form-horizontal" method="POST" action="{{ route('auth.news.store') }}" enctype="multipart/form-data">
+<form class="form-horizontal" method="POST" action="{{ route('auth.cases.store') }}" enctype="multipart/form-data">
     {{ csrf_field() }}
 
     <div class="form-group{{ $errors->has('case_head') ? ' has-error' : '' }}">

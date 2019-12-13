839f374e38d966e684bf115d5aa3557b2390eb76
Bronyrus <bronyruschannel@gmail.com>
2019-12-13 09:24:04 +0300

    header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            {{-- <li><a href="{{ route('register') }}">Register</a></li> --}}
                         @else
                             <li class="dropdown">
                                 <button type="button" class="btn btn-tc dropdown-toggle" data-toggle="dropdown"

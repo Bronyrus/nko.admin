579d075ed59961fde5c45204494155809edcefc5
Bronyrus <bronyruschannel@gmail.com>
2019-12-13 16:22:32 +0300

    Update UserWebController.php

--- app/Http/Controllers/Web/UserWebController.php
+++ app/Http/Controllers/Web/UserWebController.php
@@ -35,7 +35,49 @@ class UserWebController extends Controller
      */
     public function edit($id)
     {
-        //
+        $request->old_password = trim($request->old_password);
+        $request->old_password_confirmation = trim($request->old_password_confirmation);
+        $request->new_password = trim($request->new_password);
+        $request->new_password_confirmation = trim($request->new_password_confirmation);
+
+        $validator = Validator::make($request->all(), [
+            'old_password' => 'required|confirmed',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->route('auth.user.edit', ['id' => Auth::user()->id])
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $user = User::where('id', '=', $id)->first('password');
+
+        if (Hash::check($request->old_password, $user->password))
+        {
+            return 'suc';
+        }
+        else
+        {
+            $validator->getMessageBag()->add('old_password', 'Текущий пароль не совпадает');
+
+            if ($validator->fails()) {
+                return redirect()
+                    ->routeroute('auth.user.edit', ['id' => Auth::user()->id])
+                    ->withErrors($validator)
+                    ->withInput();
+            }
+        }
+
+        // User::create([
+        //     'name' => $request->input('mod_name'),
+        //     'email' => $request->input('email'),
+        //     'password' => Hash::make($request->input('password')),
+        //     'admin' => 0
+        // ]);
+
+        // return redirect()->route('auth.user.edit', ['id' => Auth::user()->id]);
     }
 
 }

0f19f853e5937e335734dc7a396e14c835bc1173
Bronyrus <bronyruschannel@gmail.com>
2019-12-13 14:45:01 +0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
     /**
      * @var array
      */
-    protected $guarded = ['id'];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.

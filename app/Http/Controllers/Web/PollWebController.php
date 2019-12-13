aa2bb94552ddb6abb869805398eeb80111d7c624
Bronyrus <bronyruschannel@gmail.com>
2019-12-13 11:55:51 +0300

    polls

---
+++ app/Http/Controllers/Web/PollWebController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Poll;
+use App\PollCategories;
+use App\PollQuestions;
+use App\PollQuestionAnswers;
+use App\PollQuestionAnswerUsers;
+use App\UserToPoll;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class PollWebController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('polls.pollsList', [
+        'polls' => Poll::select('*')
+            ->orderBy('created_at', 'desc')->get()
+        ]); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('polls.pollCreate', [
+            'categories' => PollCategories::select('*')
+            ->orderBy('created_at', 'desc')->get()
+        ]); 
+    }
+}

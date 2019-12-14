a76e44947503f47c488ac8d18cdc339dd18fa79b
Bronyrus <bronyruschannel@gmail.com>
2019-12-14 13:01:01 +0300

    Update Client.php

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
 class Client extends Model

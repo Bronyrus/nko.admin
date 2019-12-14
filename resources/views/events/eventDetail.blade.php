489b24ed8a90d282850a4c73b8b2dc4232942b08
Bronyrus <bronyruschannel@gmail.com>
2019-12-14 12:00:00 +0300

    Update eventDetail.blade.php

--- resources/views/events/eventDetail.blade.php
+++ resources/views/events/eventDetail.blade.php
@@ -5,7 +5,18 @@
 <div class="col-sm-12 tabs-content">
     <div class="row justify-content-center cont-m">
         <div class="col-md-12">
-            <h2>Мероприятие {{ $event->name }}</h2>
+            <h2>Мероприятие {{ $event->head }}</h2>
+            <div class="col-4 col-sm-12">
+                <p>
+                    {!! htmlspecialchars_decode($event->body) !!}
+                </p>
+            </div>
+            <div class="col-4 col-sm-12">
+                Дата проведения: {{ date('d.m.Y', strtotime($event->date_start)) }}
+            </div>
+            <div class="col-4 col-sm-12">
+                Место проведения: {{ $event->place }}
+            </div>
             <h2>Зарегистрированные участники (всего: человек(a))</h2>
             <table class="table policy-table">
                 <thead>

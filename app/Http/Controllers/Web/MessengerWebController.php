164b0eeb8ab34fb04a8a59665de84b289afaa68d
Bronyrus <bronyruschannel@gmail.com>
2019-12-16 10:48:31 +0300

    Update MessengerWebController.php

--- app/Http/Controllers/Web/MessengerWebController.php
+++ app/Http/Controllers/Web/MessengerWebController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
-class EventWebController extends Controller
+class MessengerWebController extends Controller
 {
     /**
      * Display a listing of the resource.
